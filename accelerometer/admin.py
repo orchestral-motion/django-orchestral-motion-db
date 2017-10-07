ad3868135473690ee77f5fee2c65c824eb698dd9
palewire <ben.welsh@gmail.com>
2017-10-07 14:44:06 -0700

    Simple admin

--- accelerometer/admin.py
+++ accelerometer/admin.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.contrib import admin
+from .models import Position
 
-# Register your models here.
+
+@admin.register(Position)
+class PositionAdmin(admin.ModelAdmin):
+    pass
