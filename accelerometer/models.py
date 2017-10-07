1745c5e63df225f9b708e83372bab9934c3a5a2c
palewire <ben.welsh@gmail.com>
2017-10-07 14:37:07 -0700

    Created a simple first model

--- accelerometer/models.py
+++ accelerometer/models.py
@@ -1,6 +1,21 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.db import models
 
-# Create your models here.
+
+
+class Position(models.Model):
+    """
+    A snapshot of the position of the accelerometer at a moment in time.
+    """
+    timestamp = models.DateTimeField()
+    x = models.IntegerField()
+    y = models.IntegerField()
+    z = models.IntegerField()
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ("-timestamp",)
+
+    def __unicode__(self):
+        return str(self.timestamp)
