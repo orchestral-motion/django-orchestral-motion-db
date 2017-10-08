b8a202b068a099b476aa9406de63c39847e5155e
palewire <ben.welsh@gmail.com>
2017-10-07 17:43:15 -0700

    More stuff

--- accelerometer/management/commands/loadjunkdata.py
+++ accelerometer/management/commands/loadjunkdata.py
@@ -1,6 +1,7 @@
 from django.core.management.base import BaseCommand, CommandError
 from accelerometer.models import Position
 from django.utils import timezone
+from django.db.models.signals import post_save
 import random
 
 
@@ -9,11 +10,13 @@ class Command(BaseCommand):
     help = 'Load junk data into the Position model'
 
     def handle(self, *args, **options):
+        num = 101
         # Load in junk
-        for x in range(1, 101):
-            Position.objects.create(
+        for x in range(1, num):
+            instance = Position.objects.create(
                 timestamp=timezone.now(),
                 x=random.choice(range(1, 101)),
                 y=random.choice(range(1, 101)),
                 z=random.choice(range(1, 101)),
             )
+            post_save.send(Position, instance=instance, created=True)
--- accelerometer/models.py
+++ accelerometer/models.py
@@ -1,17 +1,18 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.db import models
+from django.utils import timezone
 
 
 class Position(models.Model):
     """
     A snapshot of the position of the accelerometer at a moment in time.
     """
-    timestamp = models.DateTimeField()
+    timestamp = models.DateTimeField(default=timezone.now)
     x = models.IntegerField()
     y = models.IntegerField()
     z = models.IntegerField()
-    created = models.DateTimeField(auto_now_add=True)
+    created = models.DateTimeField(default=timezone.now)
 
     class Meta:
         ordering = ("-timestamp",)
