b60d2ea803fbec3bf27b61d4ed7cd0386b173087
palewire <ben.welsh@gmail.com>
2017-10-07 18:21:57 -0700

    Like this?

--- project/settings.py
+++ project/settings.py
@@ -81,7 +81,10 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+
+DATABASES = {
+    'default': db_from_env
+}
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
